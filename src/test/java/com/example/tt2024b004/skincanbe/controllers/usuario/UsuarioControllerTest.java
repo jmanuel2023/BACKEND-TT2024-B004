f359c41c6a5b8f0a5a02ceafbd089e257ad73d99
jmanuel2023 <128843828+jmanuel2023@users.noreply.github.com>
2024-11-07 10:42:34 -0600

    Update UsuarioControllerTest.java

--- src/test/java/com/example/tt2024b004/skincanbe/controllers/usuario/UsuarioControllerTest.java
+++ src/test/java/com/example/tt2024b004/skincanbe/controllers/usuario/UsuarioControllerTest.java
@@ -19,7 +19,11 @@ public class UsuarioControllerTest {
     @MockBean
     private UsuarioService usuarioService;
 
+    @MockBean
+    private TokenVerificacion tokenVerificacionService;
+
     private Usuario usuario;
+    private List<Especialista> especialista;
 
     @BeforeEach
     void setUp(){
@@ -31,35 +35,117 @@ public class UsuarioControllerTest {
         usuario.setEdad(25);
         usuario.setPassword("Octavitito1234");
         usuario.setStatus("Pendiente");
+
+        Especialista especialista1 = new Especialista();
+        especialista1.setNombre("Juan");
+        especialista1.setCedula("12345");
+
+        Especialista especialista2 = new Especialista();
+        especialista2.setNombre("Pedrito");
+        especialista2.setCedula("67890");
+
+        especialistas = Arrays.asList(especialista1, especialista2);
     }
 
     @Test
-    void testCrearUsuario() {
+    void testCrearUsuario() throws Exception{
+        Map<String,Object> peticion = new HashMap<>();
+        peticion.put("nombre": "Octavio");
+        peticion.put("id": 1L);
+        peticion.put("apellidos":"Lopez");
+        peticion.put("correo":"octavio@gmail.com");
+        peticion.put("password": "Octavitito1234");
+        peticion.put("status":"Pendiente");
+        peticion.put("edad":25);
+        Mockito.when(usuarioService.crearUsuario(peticion)).thenReturn(usuario);
 
+        // Realizar la solicitud POST y verificar la respuesta
+        mockMvc.perform(post("/crear")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(new ObjectMapper().writeValueAsString(peticion)))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.nombre").value("Octavio"))
+            .andExpect(jsonPath("$.id").value(1L))
+            .andExpect(jsonPath("$.apellidos").value("Lopez"))
+            .andExpect(jsonPath("$.correo").value("octavio@gamil.com"))
+            .andExpect(jsonPath("$.edad").value(25))
+            .andExpect(jsonPath("$.password").value("Octavitito1234"))
+            .andExpect(jsonPath("$.status").value("Pendiente"));
     }
 
     @Test
-    void testForgotPassword() {
+    void testForgotPassword() throws Exception{
+        Map<String,String> request = new HashMap<>();
+        peticion.put("correo":"otrousuario@gmail.com");
+        Mockito.when(usuarioService.(request)).thenReturn(null);
 
+        // Realizar la solicitud POST y verificar la respuesta
+        mockMvc.perform(post("/forgotpassword")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(new ObjectMapper().writeValueAsString(request)))
+            .andExpect(status().isNotFound())
+            .andExpect(content().string("Correo no encontrado!"));
     }
 
     @Test
-    void testObtenerEspecialistasPorNombreYCedula() {
+    void testObtenerEspecialistasPorNombreYCedula() throws Exception {
+        String filtro = "Juan";
+        when(usuarioService.obtenerEspecialistasPorNomYCed(filtro)).thenReturn(especialistas);
 
+        mockMvc.perform(get("/specialistFilter/{filtro}", filtro)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].nombre").value("Juan"))
+                .andExpect(jsonPath("$[0].cedula").value("12345"))
+                .andExpect(jsonPath("$[1].nombre").value("Maria"))
+                .andExpect(jsonPath("$[1].cedula").value("67890"));
+        
     }
 
     @Test
-    void testObtenerTodosLosEspecialistas() {
-
+    void testObtenerTodosLosEspecialistas() throws Exception {
+        when(usuarioService.obtenerTodosLosEspecialistas()).thenReturn(especialistas);
+         mockMvc.perform(get("/specialistFilter")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].nombre").value("Juan"))
+                .andExpect(jsonPath("$[0].cedula").value("12345"))
+                .andExpect(jsonPath("$[1].nombre").value("Maria"))
+                .andExpect(jsonPath("$[1].cedula").value("67890"));
     }
 
     @Test
-    void testResetPassword() {
+    void testResetPassword() throws Exception {
+        Map<String, String> request = new HashMap<>();
+        request.put("token", "validToken");
+        request.put("password", "newPassword");
 
+        // Configurar el comportamiento del mock estático para JwtUtil
+        try (MockedStatic<JwtUtil> mockedJwtUtil = mockStatic(JwtUtil.class)) {
+            mockedJwtUtil.when(() -> JwtUtil.validatePasswordResetToken("validToken")).thenReturn(true);
+            mockedJwtUtil.when(() -> JwtUtil.getEmailFromPasswordResetToken("validToken")).thenReturn("octavio@gamil.com");
+
+            // Configurar el comportamiento del servicio mock
+            when(usuarioService.resetPassword(request)).thenReturn(usuario);
+
+            // Realizar la solicitud POST y verificar la respuesta
+            mockMvc.perform(post("/reset-password")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(new ObjectMapper().writeValueAsString(request)))
+                    .andExpect(status().isOk())
+                    .andExpect(content().string("Contraseña actualizada correctamente"));
     }
 
     @Test
-    void testValidarCorreo() {
+    void testValidarCorreo() throws Exception{
+        String token = "validToken";
+        when(tokenVerificacionService.validarToken(token)).thenReturn("Correo validado correctamente");
+
+        mockMvc.perform(get("/validar")
+                .param("token", token)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Correo validado correctamente"));
 
     }
 }
